83b6f16cd2e039ff28f022ffc018076adf91c6c9
Cai Chen <achen2304@gmail.com>
2025-08-28 16:54:46 -0500

    fix: more jsdocs

--- src/components/common/news-card/types/news-card.ts
+++ src/components/common/news-card/types/news-card.ts
@@ -1,3 +1,13 @@
+/**
+ * Props for the NewsCard component
+ * @param {string} image - The image of the news card
+ * @param {string} source - The source of the news card
+ * @param {string} date - The date of the news card
+ * @param {string} headline - The headline of the news card
+ * @param {boolean} isDark - Whether the news card is dark
+ * @param {string} className - The class name of the news card
+ * @param {string} link - The link of the news card
+ */
 export default interface NewsCardProps {
   image: string;
   source: string;
--- src/components/common/placeholder-image/types/placeholder-image.ts
+++ src/components/common/placeholder-image/types/placeholder-image.ts
@@ -1,3 +1,13 @@
+/**
+ * Props for the PlaceholderImage component
+ * @param {string} src - The source of the placeholder image
+ * @param {string} alt - The alt text of the placeholder image
+ * @param {number} width - The width of the placeholder image
+ * @param {number} height - The height of the placeholder image
+ * @param {string} className - The class name of the placeholder image
+ * @param {string} fallbackText - The fallback text of the placeholder image
+ * @param {boolean} isDark - Whether the placeholder image is dark
+ */
 export default interface PlaceholderImageProps {
   src: string;
   alt: string;
