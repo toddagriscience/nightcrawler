c2e4949a0ac39ad295fa9dc97ec9f2f54f907321
Cai Chen <achen2304@gmail.com>
2025-09-01 00:23:04 -0500

    feat: separated i18n translations for better collaboration & updated docs\

---
+++ src/i18n/message-files.ts
@@ -0,0 +1,14 @@
+// Copyright Todd LLC, All rights reserved.
+
+/**
+ * A list of all message files
+ * @returns {string[]} - The list existing message files
+ */
+export const messageFiles = [
+  'common',
+  'header',
+  'footer',
+  'homepage',
+  'who-we-are',
+  'contact',
+];
