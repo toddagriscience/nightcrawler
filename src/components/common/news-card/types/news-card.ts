1becc9bd3726cce6da66bbbf541b2a3f0b517f05
kurealnum <oscar.gaske.cs@gmail.com>
2025-09-14 16:17:14 -0400

    docs: documented

--- src/components/common/news-card/types/news-card.ts
+++ src/components/common/news-card/types/news-card.ts
@@ -2,24 +2,27 @@
 
 /**
  * Props for the NewsCard component
- * @param {string} image - The image of the news card
- * @param {string} source - The source of the news card
+ * @param {string} title - The title of the news card
+ * @param {string} excerpt - The headline of the news card
+ * @param {string} source - The person/entity who wrote this news article. Examples: Todd, Bob Bobberson, Journal, CNN.
  * @param {string} date - The date of the news card
- * @param {string} headline - The headline of the news card
  * @param {boolean} isDark - Whether the news card is dark
  * @param {string} className - The class name of the news card
- * @param {string} link - The link of the news card
+ * @param {string} slug - The slug of the news article. Examples: /news/articles/{my-awesome-article}
+ * @param {Object} image - The object containing the information for the thumbnail
+ * @param {string} image.url - The URL for the thumbnail
+ * @param {string} image.alt - The alt text for the thumbnail. This may not always be required.
  */
 export default interface NewsCardProps {
   title: string;
-  image: {
-    url: string;
-    alt: string;
-  };
   excerpt: string;
   source?: string;
   date: string;
   isDark?: boolean;
   className?: string;
   slug?: string;
+  image: {
+    url: string;
+    alt: string;
+  };
 }
