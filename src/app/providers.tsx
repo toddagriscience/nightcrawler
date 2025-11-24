cb184e27f2c2a1b5be7598ac783478bf3051cbe8
Oscar <116363763+kurealnum@users.noreply.github.com>
2025-11-24 10:45:15 -0500

    feat: change posthog to cookieless (#224)
    Hotfix

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -13,6 +13,7 @@ export function PostHogProvider({ children }: { children: React.ReactNode }) {
         process.env.NEXT_PUBLIC_POSTHOG_HOST || 'https://us.i.posthog.com',
       person_profiles: 'identified_only', // or 'always' to create profiles for anonymous users as well
       defaults: '2025-05-24',
+      cookieless_mode: 'always',
     });
   }, []);
 
