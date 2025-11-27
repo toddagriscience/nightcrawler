7de7526f3e430baf367b5627e4b88fe80619b0a4
kurealnum <oscar.gaske.cs@gmail.com>
2025-11-27 00:36:51 -0500

    refactor: improve protected.tsx logic

--- src/components/auth/protected.tsx
+++ src/components/auth/protected.tsx
@@ -4,8 +4,24 @@
 import { checkAuthenticated } from '@/lib/auth';
 import { redirect } from 'next/navigation';
 import { useEffect, useState } from 'react';
+import { Spinner } from '../ui/spinner';
+import { FadeIn } from '../common';
 
-export default function Protected({ children }: { children: React.ReactNode }) {
+/** A HOC for routes that require authentication. This is a form of optimistic authentication -- i.e. don't trust it with sensitive data.
+ *
+ * @param {Object} props
+ * @param {React.ReactNode} props.children - The child ReactNodes to be returned
+ * @param {React.ReactNode} props.loadingElement - The element to display while waiting for a user to be authenticated. Default is an absolutely-positioned spinner in the center of the screen.
+ * @param {string} props.redirectTo - The location to redirect to if the user fails authentication. Defaults to '/en'*/
+export default function Protected({
+  children,
+  loadingElement,
+  redirectTo = '/en',
+}: {
+  children: React.ReactNode;
+  loadingElement?: React.ReactNode;
+  redirectTo?: string;
+}) {
   const [isAuth, setIsAuth] = useState(false);
   const [isBusy, setIsBusy] = useState(true);
 
@@ -19,12 +35,22 @@ export default function Protected({ children }: { children: React.ReactNode }) {
   }, [setIsAuth, setIsBusy]);
 
   if (isBusy) {
-    return <p>Loading</p>;
+    if (loadingElement != null) {
+      return loadingElement;
+    } else {
+      return (
+        <div className="relative h-screen">
+          <FadeIn duration={0.2}>
+            <Spinner className="absolute inset-0 m-auto w-12 h-12" />
+          </FadeIn>
+        </div>
+      );
+    }
   }
 
   if (isAuth === true) {
     return children;
   }
 
-  redirect('login');
+  redirect(redirectTo);
 }
