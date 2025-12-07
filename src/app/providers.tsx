4dd934f1df91748b799d4e21e09a166c3c0e0cdb
kurealnum <oscar.gaske.cs@gmail.com>
2025-12-06 20:15:16 -0500

    fix: one more try, fully default settings

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -7,8 +7,6 @@ import { PostHogProvider as PHProvider } from 'posthog-js/react';
 if (typeof window !== 'undefined') {
   posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY!, {
     api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST,
-    person_profiles: 'always', // or 'always' to create profiles for anonymous users as well
-    cookieless_mode: 'on_reject',
   });
 }
 
