73663f349b3181da49ec5c95c7f039c3c571597e
Oscar <116363763+kurealnum@users.noreply.github.com>
2025-12-09 23:42:55 -0500

    feat: allow certain pages for all users (#278)
    Hotfix for allowing authenticated users to access accessibility pages.
    This also fixes an issue with #277 that I didn't catch.

---
+++ src/middleware/all-users-page.ts
@@ -0,0 +1,19 @@
+// Copyright Todd Agriscience, Inc. All rights reserved.
+
+import { isRouteInternationalized } from '@/lib/routing';
+import { NextRequest } from 'next/server';
+
+/** A list of routes that all users need to be able to access. These should be treated as internationalized routes. See the `isAllUserRoute` function for more information. */
+const allUserRoutesIntl = ['privacy', 'accessibility'];
+
+/** Handles routes that all users need access to. An "all users" page is one that all users need to be able to access, whether authenticated or not. Due to the nature of auth routing in Nightcrawler (authenticated users are redirected away from anything localized, unauthenticated users are redirected away from anything un-localized), these routes need to be handled manually. You can find a list of these routes in the `allUserRoutes` constant. */
+export default async function isAllUserRoute(request: NextRequest) {
+  const { pathname } = request.nextUrl;
+
+  if (isRouteInternationalized(pathname)) {
+    const unIntlRoute = pathname.split('/')[2];
+    return allUserRoutesIntl.includes(unIntlRoute);
+  }
+
+  return false;
+}
