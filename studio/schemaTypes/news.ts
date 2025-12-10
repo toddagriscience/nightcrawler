5ae13abc1a190d051e83ba706480a73f13497967
kurealnum <oscar.gaske.cs@gmail.com>
2025-12-10 17:56:12 -0500

    feat: updated /en/news to accomadate sanity, handled off site articles

--- studio/schemaTypes/news.ts
+++ studio/schemaTypes/news.ts
@@ -14,12 +14,14 @@ export default defineType({
       title: 'Title',
       type: 'string',
       validation: (rule) => rule.required(),
+      description:
+        'Only the title of the article. Nothing extra (such as `Your title here | Todd Agriscience`) is required.',
     }),
     defineField({
       name: 'slug',
       title: 'Slug',
       type: 'slug',
-      description: 'A slug is required for the post to show up in the preview',
+      description: 'A slug is required for the post to be accessible from `toddagriscience.com`',
       options: {
         source: 'title',
         maxLength: 96,
@@ -37,6 +39,7 @@ export default defineType({
       name: 'content',
       title: 'Content',
       type: 'array',
+      validation: (rule) => rule.required(),
       of: [
         {
           type: 'block',
@@ -61,6 +64,44 @@ export default defineType({
       name: 'summary',
       title: 'Summary',
       type: 'string',
+      validation: (rule) => rule.required(),
+      description: 'A brief description of the article. Used for SEO.',
+    }),
+    defineField({
+      name: 'thumbnail',
+      title: 'Thumbnail',
+      type: 'image',
+      description:
+        'A thumbnail for the article on the `/en/news` page. Not currently used for SEO.',
+      fields: [
+        {
+          name: 'alt',
+          type: 'string',
+          title: 'Alt Text',
+          validation: (rule) => rule.required(),
+        },
+      ],
+    }),
+    defineField({
+      name: 'offSiteUrl',
+      title: 'Off Site Link',
+      type: 'url',
+      description:
+        'If a URL is present in this field, no news page will exist for this article. Specifically, navigating to a URL such as `/en/news/{your-slug}` will redirect to this URL.',
+    }),
+    defineField({
+      name: 'isFeatured',
+      title: 'Featured',
+      type: 'boolean',
+      description: 'Featured articles are displayed on the featured section of `/en/news`.',
+    }),
+    defineField({
+      name: 'source',
+      title: 'Source',
+      type: 'string',
+      initialValue: 'Todd',
+      description:
+        'The source of the article. Defaults to "Todd" for articles listed on `toddagriscience.com`.',
     }),
   ],
   preview: {
