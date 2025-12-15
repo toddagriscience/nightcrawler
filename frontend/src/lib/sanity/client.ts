16e8ca1c3cf9a7751d0e0f6a20df12cc4b17b5da
kurealnum <oscar.gaske.cs@gmail.com>
2025-12-15 10:49:00 -0500

    fix: actions x5

--- frontend/src/lib/sanity/client.ts
+++ frontend/src/lib/sanity/client.ts
@@ -3,12 +3,8 @@
 import { createClient } from 'next-sanity';
 
 export const client = createClient({
-  projectId:
-    process.env.NEXT_PUBLIC_SANITY_PROJECT_ID ||
-    'Missing env variable: NEXT_PUBLIC_SANITY_PROJECT_ID',
-  dataset:
-    process.env.NEXT_PUBLIC_SANITY_DATASET ||
-    'Missing env variable: NEXT_PUBLIC_SANITY_DATASET',
+  projectId: '3x7sixjh',
+  dataset: 'production',
   apiVersion: '2024-01-01',
   useCdn: true,
 });
