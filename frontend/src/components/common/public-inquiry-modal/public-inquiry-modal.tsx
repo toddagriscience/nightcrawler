19ff4afcdd018d75f00ec6b598ba382596c2463f
allykeightley <allysonkeightley@outlook.com>
2026-01-21 17:14:04 -0800

    feat: add public inquiry ticket modal

---
+++ frontend/src/components/common/public-inquiry-modal/public-inquiry-modal.tsx
@@ -0,0 +1,92 @@
+// Copyright © Todd Agriscience, Inc. All rights reserved.
+
+'use client';
+
+import { Button } from '@/components/ui/button';
+import {
+    Dialog,
+    DialogContent,
+    DialogFooter,
+    DialogHeader,
+    DialogTitle,
+    DialogTrigger,
+} from '@/components/ui/dialog';
+import Link from 'next/link';
+import { useState } from 'react';
+import { PublicInquiryOption } from './types/public-inquiry-modal';
+
+/**
+ * Public Inquiry modal. Allows users to submit a public inquiry support ticket.
+ *
+ * @param {React.ReactNode} trigger - The element that opens the modal. Optional, defaults to a button.
+ * @returns {JSX.Element} - The Public Inquiry modal with related logic.
+ */
+export default function PublicInquiryModal({
+  trigger = undefined,
+}: {
+  trigger?: React.ReactNode;
+}) {
+  const [isOpen, setIsOpen] = useState(false);
+  const options: PublicInquiryOption[] = [
+    { label: 'Forgot Email', href: '/support' },
+    { label: 'Forgot Password', href: '/forgot-password' },
+    { label: 'Contact Support', href: '/support' },
+  ];
+
+  return (
+    <Dialog
+      open={isOpen}
+      onOpenChange={isOpen ? () => setIsOpen(false) : () => {}}
+    >
+      <DialogTrigger asChild>
+        {trigger ? (
+          <Button
+            className="m-0 h-min w-auto !justify-start p-0 text-base font-medium hover:cursor-pointer hover:no-underline"
+            onClick={() => setIsOpen(true)}
+            asChild
+          >
+            {trigger}
+          </Button>
+        ) : (
+          <Button
+            type="button"
+            variant={'outline'}
+            onClick={() => setIsOpen(true)}
+            className="hover:cursor-pointer"
+          >
+            Help
+          </Button>
+        )}
+      </DialogTrigger>
+      {/* <FadeIn duration={0.6}> */}
+        <DialogContent className="sm:max-w-[430px] gap-8">
+          <DialogHeader className="gap-1">
+            <DialogTitle className="text-xl font-bold leading-tight">
+              What can we help with?
+            </DialogTitle>
+          </DialogHeader>
+          <div className="border-b border-[#2A2727]/60 divide-y divide-[#2A2727]/60">
+            {options.map((option) => (
+              <Link
+                key={option.label}
+                href={option.href}
+                className="flex w-full items-center justify-between px-4 py-4 text-left text-base font-medium text-foreground transition hover:bg-[var(--background-secondary)]/20 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-[-2px] focus-visible:outline-primary hover:transition-all hover:duration-300 hover:ease-in-out"
+              >
+                {option.label}
+              </Link>
+            ))}
+          </div>
+          <DialogFooter className="sm:justify-center">
+            <Button
+              variant="outline"
+              className="w-full rounded-full px-6 py-6 text-base font-bold hover:cursor-pointer hover:bg-[var(--popover-foreground)] hover:text-[var(--primary-foreground)] focus:outline-none hover:transition-all hover:duration-300 hover:ease-in-out"
+              onClick={() => setIsOpen(false)}
+            >
+              Cancel
+            </Button>
+          </DialogFooter>
+        </DialogContent>
+      {/* </FadeIn> */}
+    </Dialog>
+  );
+}
---
+++ frontend/src/components/common/public-inquiry-modal/types/public-inquiry-modal.ts
@@ -0,0 +1,6 @@
+// Copyright © Todd Agriscience, Inc. All rights reserved.
+
+export type PublicInquiryOption = {
+    label: 'Forgot Email' | 'Forgot Password' | 'Contact Support';
+    href: string;
+  };
