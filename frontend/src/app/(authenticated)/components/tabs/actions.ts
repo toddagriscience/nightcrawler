372056ec8a1e98d2422595ea6a3d5ae3c9581efb
Oscar <116363763+kurealnum@users.noreply.github.com>
2026-02-03 16:00:11 -0500

    feat: add action for deleting a tab (#476)
    ## Description
    
    Fixes #453
    
    ## Checklist
    
    - [ ] You've included unit or integration tests for your change, where
    applicable.
    - [ ] You've included inline docs for your change, where applicable.
    - [ ] Any components that you've modified are accessible.
    - [ ] You've used [conventional
    commits](https://www.conventionalcommits.org/en/v1.0.0/) where
    appropriate

--- frontend/src/app/(authenticated)/components/tabs/actions.ts
+++ frontend/src/app/(authenticated)/components/tabs/actions.ts
@@ -68,3 +68,97 @@ export default async function updateTabName(
     return { error: 'Failed to update tab' };
   }
 }
+
+/** Deletes a tab given the name or the id of the tab.
+ *
+ * @param name - Name of the tab (AKA the name of the management zone)
+ * @param tabId - The literal id of the tab
+ * */
+export async function deleteTab(
+  name?: string,
+  tabId?: number
+): Promise<ActionResponse> {
+  try {
+    const result = await getAuthenticatedInfo();
+
+    if ('error' in result) {
+      return result;
+    }
+
+    if (!result.id || !result.farmId) {
+      return { error: 'No user or farm ID provided in database' };
+    }
+
+    const userId = result.id;
+    const farmId = result.farmId;
+
+    if (!name && !tabId) {
+      return { error: 'Please provide either oldName or tabId' };
+    }
+
+    if (name) {
+      const [zone] = await db
+        .select({ id: managementZone.id })
+        .from(managementZone)
+        .where(
+          and(eq(managementZone.name, name), eq(managementZone.farmId, farmId))
+        );
+
+      await db
+        .delete(tab)
+        .where(and(eq(tab.user, userId), eq(tab.managementZone, zone.id)));
+    } else if (tabId) {
+      await db.delete(tab).where(eq(tab.id, tabId));
+    }
+
+    return { error: null };
+  } catch (error) {
+    if (error instanceof Error) {
+      return { error: error.message };
+    }
+    return { error: 'Failed to delete tab' };
+  }
+}
+
+/** Create tab, given a management zone's id. A maximum of 8 tabs can be created.
+ * @param managementZoneId - The ID of the management zone that this tab is being created for*/
+export async function createTab(
+  managementZoneId: number
+): Promise<ActionResponse> {
+  const maxTabs = 8;
+
+  try {
+    const result = await getAuthenticatedInfo();
+
+    if ('error' in result) {
+      return result;
+    }
+
+    if (!result.id) {
+      return { error: 'No user id present in database' };
+    }
+
+    const userId = result.id;
+
+    // Check to see if user has the max tabs saved
+    const currentTabs = await db
+      .select({})
+      .from(tab)
+      .where(eq(tab.user, userId));
+
+    if (currentTabs.length >= maxTabs) {
+      return { error: `Can't create more than ${maxTabs} tabs` };
+    }
+
+    await db
+      .insert(tab)
+      .values({ managementZone: managementZoneId, user: userId });
+
+    return { error: null };
+  } catch (error) {
+    if (error instanceof Error) {
+      return { error: error.message };
+    }
+    return { error: 'Failed to delete tab' };
+  }
+}
