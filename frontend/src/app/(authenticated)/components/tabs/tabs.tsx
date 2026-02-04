329f764372630a3ac74f069bad04c828e281e14b
kurealnum <oscar.gaske.cs@gmail.com>
2026-02-04 17:25:51 -0500

    feat: add ability to change inputs

--- frontend/src/app/(authenticated)/components/tabs/tabs.tsx
+++ frontend/src/app/(authenticated)/components/tabs/tabs.tsx
@@ -7,7 +7,7 @@ import { NamedTab } from './types';
 import { Button } from '@/components/ui';
 import { getTabHash } from './utils';
 import { ManagementZoneSelect, UserSelect } from '@/lib/types/db';
-import {
+import updateTabName, {
   createTab as createTabAction,
   deleteTab as deleteTabAction,
 } from './actions';
@@ -30,17 +30,17 @@ export default function PlatformTabs({
   children: React.ReactNode;
 }) {
   const router = useRouter();
-  const [tab, setTab] = useState(
+  const [curTab, setCurTab] = useState(
     currentTabs.length !== 0 ? getTabHash(currentTabs[0]) : 'home'
   );
 
-  async function setTabHelper({ newTabId }: { newTabId?: NamedTab }) {
+  async function setCurTabHelper({ newTabId }: { newTabId?: NamedTab }) {
     if (currentTabs.length === 0) {
-      setTab('home');
+      setCurTab('home');
     } else if (newTabId) {
-      setTab(getTabHash(newTabId));
+      setCurTab(getTabHash(newTabId));
     } else {
-      setTab(getTabHash(currentTabs[0]));
+      setCurTab(getTabHash(currentTabs[0]));
     }
   }
 
@@ -57,7 +57,7 @@ export default function PlatformTabs({
 
     router.refresh();
 
-    setTabHelper({
+    setCurTabHelper({
       newTabId: {
         name: managementZoneName || 'Untitled Zone',
         managementZone: managementZoneId,
@@ -78,18 +78,29 @@ export default function PlatformTabs({
 
     router.refresh();
 
-    setTabHelper({});
+    setCurTabHelper({});
+  }
+
+  async function updateTab(newName: string, tabId: number) {
+    const result = await updateTabName({ newName, tabId });
+
+    if (result.error) {
+      logger.error(result.error);
+      return;
+    }
+
+    router.refresh();
   }
 
   return (
-    <Tabs value={tab}>
+    <Tabs value={curTab}>
       <div className="absolute top-4 left-40 max-w-[70vw] min-[107rem]:right-0 min-[107rem]:left-0 min-[107rem]:m-auto min-[107rem]:w-[107rem] min-[107rem]:max-w-350">
         <TabsList className="flex flex-row flex-nowrap justify-start gap-2 bg-transparent">
           {!currentTabs.length ? (
             <TabsTrigger
               value="home"
               className="w-36 max-w-56 flex-1 truncate overflow-hidden border-none text-left text-ellipsis whitespace-nowrap data-[state=active]:bg-gray-200"
-              onClick={() => setTabHelper({})}
+              onClick={() => setCurTabHelper({})}
             >
               Home
             </TabsTrigger>
@@ -99,11 +110,12 @@ export default function PlatformTabs({
                 className="pl-2 group max-w-36 min-w-36 truncate border-none data-[state=active]:bg-gray-200 group"
                 key={tab.id}
                 value={getTabHash(tab)}
-                onClick={() => setTabHelper({ newTabId: tab })}
+                onClick={() => setCurTabHelper({ newTabId: tab })}
               >
                 <input
-                  className="max-w-[100%] cursor-pointer text-center ring-0 outline-none group-data-[state=active]:pointer-events-none focus:ring-0 focus:outline-none"
+                  className="max-w-[100%] cursor-pointer text-center ring-0 outline-none group-data-[state=active]:pointer-events-auto pointer-events-none focus:ring-0 focus:outline-none"
                   defaultValue={tab.name || `Untitled Zone ${index}`}
+                  onChange={(e) => updateTab(e.target.value, tab.id)}
                 />
                 {/** This isn't the best solution, but it's the easiest way to nest buttons. Getting the entire background to render with a wrapping div via data-[state=active] is just a pain */}
                 <div
