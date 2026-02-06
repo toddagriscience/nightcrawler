285540a8e155d0af6889a415b5d6592e142dd741
kurealnum <oscar.gaske.cs@gmail.com>
2026-02-06 14:05:33 -0500

    refactor: replace wordmark with svg

--- frontend/src/components/common/wordmark/todd-wordmark.tsx
+++ frontend/src/components/common/wordmark/todd-wordmark.tsx
@@ -1,6 +1,8 @@
 // Copyright © Todd Agriscience, Inc. All rights reserved.
 
 import Link from 'next/link';
+import Wordmark from '@public/wordmark.svg';
+import Image from 'next/image';
 
 /** Small helper for the TODD logo.
  *
@@ -10,10 +12,10 @@ export default function ToddHeader({ className = '' }: { className?: string }) {
   return (
     <Link
       href="/"
-      className={`wordmark uppercase text-4xl leading-none ${className}`}
+      className={`wordmark leading-none ${className}`}
       data-testid="wordmark-link"
     >
-      TODD
+      <Image src={Wordmark} alt="" />
     </Link>
   );
 }
