256273ba19341a062d52df232b6119d2ae111abf
kurealnum <oscar.gaske.cs@gmail.com>
2026-02-12 10:02:27 -0500

    fix: unique logic

--- frontend/src/lib/db/schema/widget.ts
+++ frontend/src/lib/db/schema/widget.ts
@@ -1,6 +1,13 @@
 // Copyright © Todd Agriscience, Inc. All rights reserved.
 
-import { integer, jsonb, pgEnum, pgTable, serial } from 'drizzle-orm/pg-core';
+import {
+  integer,
+  jsonb,
+  pgEnum,
+  pgTable,
+  serial,
+  unique,
+} from 'drizzle-orm/pg-core';
 import { tab } from './tab';
 import { LayoutItem } from 'react-grid-layout';
 
@@ -8,11 +15,15 @@ import { LayoutItem } from 'react-grid-layout';
 export const widgetEnum = pgEnum('widgets', ['macroRadar']);
 
 /** Active widgets on a given tab. A widget will need to be deleted from this table if it is to be removed from the user's UI. */
-export const widget = pgTable('widget', {
-  id: serial().primaryKey().notNull(),
-  tab: integer()
-    .references(() => tab.id, { onDelete: 'cascade' })
-    .notNull(),
-  name: widgetEnum().notNull(),
-  widgetMetadata: jsonb().$type<LayoutItem>(),
-});
+export const widget = pgTable(
+  'widget',
+  {
+    id: serial().primaryKey().notNull(),
+    tab: integer()
+      .references(() => tab.id, { onDelete: 'cascade' })
+      .notNull(),
+    name: widgetEnum().notNull(),
+    widgetMetadata: jsonb().$type<LayoutItem>(),
+  },
+  (t) => [unique().on(t.tab, t.name)]
+);
