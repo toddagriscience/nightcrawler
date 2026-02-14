767b73e1e2940630b7b52fe9a2dec365c9fcbfd8
allykeightley <allysonkeightley@outlook.com>
2026-02-13 18:02:06 -0800

    style: update suggested photo size for sanity

--- sanity-studio/schemaTypes/governance-profiles.ts
+++ sanity-studio/schemaTypes/governance-profiles.ts
@@ -8,29 +8,29 @@ import {defineField, defineType} from 'sanity'
 export default defineType({
   name: 'governance-profiles',
   title: 'Governance Profiles',
-  description: 'A collection of profiles for the team, advisors, and governance structure at Todd Agriscience.',
+  description:
+    'A collection of profiles for the team, advisors, and governance structure at Todd Agriscience.',
   icon: UsersIcon,
   type: 'document',
   fields: [
     defineField({
-        name: 'slug',
-        title: 'Slug',
-        type: 'slug',
-        description: 'A slug is required for the post to be accessible from `toddagriscience.com`',
-        options: {
-          source: 'title',
-          maxLength: 96,
-          isUnique: (value, context) => context.defaultIsUnique(value, context),
-        },
-        validation: (rule) => rule.required(),
+      name: 'slug',
+      title: 'Slug',
+      type: 'slug',
+      description: 'A slug is required for the post to be accessible from `toddagriscience.com`',
+      options: {
+        source: 'title',
+        maxLength: 96,
+        isUnique: (value, context) => context.defaultIsUnique(value, context),
+      },
+      validation: (rule) => rule.required(),
     }),
     defineField({
       name: 'name',
       title: 'Name',
       type: 'string',
       validation: (rule) => rule.required(),
-      description:
-        'Full name of the person. Do not include any titles or suffixes.',
+      description: 'Full name of the person. Do not include any titles or suffixes.',
     }),
     defineField({
       name: 'title',
@@ -40,38 +40,38 @@ export default defineType({
       description: 'The title of the person. This is typically their official job title.',
     }),
     defineField({
-        name: 'profileImage',
-        title: 'Profile Image',
-        type: 'image',
-        description:
-          'A professional headshot for the person. This image appears on the governance profiles page. Suggested size: 725x775px.',
-        validation: (rule) => rule.required(),
-        fields: [
-          {
-            name: 'alt',
-            type: 'string',
-            title: 'Alt Text',
-            description: 'The alt text for the headshot image. This is used for accessibility.',
-            validation: (rule) => rule.required(),
-          },
-        ],
-      }),
-      defineField({
-        name: 'quote',
-        title: 'Quote',
-        type: 'string',
-        description: 'A quote from the person. This is typically their official quote or words of wisdom for their profile page. Please use " " around the quote.',
-      }),
+      name: 'profileImage',
+      title: 'Profile Image',
+      type: 'image',
+      description:
+        'A professional headshot for the person. This image appears on the governance profiles page. Suggested size: 500x600px.',
+      validation: (rule) => rule.required(),
+      fields: [
+        {
+          name: 'alt',
+          type: 'string',
+          title: 'Alt Text',
+          description: 'The alt text for the headshot image. This is used for accessibility.',
+          validation: (rule) => rule.required(),
+        },
+      ],
+    }),
+    defineField({
+      name: 'quote',
+      title: 'Quote',
+      type: 'string',
+      description:
+        'A quote from the person. This is typically their official quote or words of wisdom for their profile page. Please use " " around the quote.',
+    }),
     defineField({
       name: 'backstory',
       title: 'Backstory',
       type: 'array',
-      description: 'A list of paragraphs for the backstory. This is used to display the backstory of the person.',
+      description:
+        'A list of paragraphs for the backstory. This is used to display the backstory of the person.',
       validation: (rule) =>
         rule.custom((value, context) => {
-          return Array.isArray(value) && value.length > 0
-            ? true
-            : 'Backstory is required.'
+          return Array.isArray(value) && value.length > 0 ? true : 'Backstory is required.'
         }),
       of: [
         {
@@ -80,22 +80,21 @@ export default defineType({
       ],
     }),
     defineField({
-        name: 'vision',
-        title: 'Vision',
-        type: 'array',
-        description: 'A list of paragraphs for the vision statement of the person. This is used to display the vision of the person.',
-        validation: (rule) =>
-          rule.custom((value, context) => {
-            return Array.isArray(value) && value.length > 0
-              ? true
-              : 'Vision is required.'
-          }),
-        of: [
-          {
-            type: 'block',
-          },
-        ],
-      }),
+      name: 'vision',
+      title: 'Vision',
+      type: 'array',
+      description:
+        'A list of paragraphs for the vision statement of the person. This is used to display the vision of the person.',
+      validation: (rule) =>
+        rule.custom((value, context) => {
+          return Array.isArray(value) && value.length > 0 ? true : 'Vision is required.'
+        }),
+      of: [
+        {
+          type: 'block',
+        },
+      ],
+    }),
     defineField({
       name: 'email',
       title: 'Email ',
