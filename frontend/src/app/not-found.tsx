d3589069300d45968d126288a5a1434c20c6989a
Ishmeet Kaur <kishmeet2905@gmail.com>
2026-02-24 13:53:26 -0500

    Fix formatting for not-found

--- frontend/src/app/not-found.tsx
+++ frontend/src/app/not-found.tsx
@@ -20,37 +20,42 @@ export default async function NotFound() {
   return (
     <NextIntlClientProvider locale={locale} messages={messages}>
       <SmoothScroll>
-
-      	<div className={`w-screen min-h-screen flex flex-col ${
-    user ? 'bg-background-platform' : 'bg-background'
-  }`}>
-        {user ? <AuthenticatedHeader /> : <Header />}
-        <FadeIn>
-		<div className="min-h-[calc(100vh-64px)] flex flex-col justify-center items-center max-w-[500px] w-[90vw] mx-auto">             <h1 className="mb-8 text-4xl text-center">{t('notFound.title')}</h1>
-            <p className="text-center">
-              {t.rich('notFound.message', {
-                home: (chunks) => (
-                  <Link
-                    href="/"
-                    className="underline underline-offset-4 hover:opacity-80"
-                  >
-                    {chunks}
-                  </Link>
-                ),
-                news: (chunks) => (
-                  <Link
-                    href="/news"
-                    className="underline underline-offset-4 hover:opacity-80"
-                  >
-                    {chunks}
-                  </Link>
-                ),
-              })}
-            </p>
-          </div>
-        </FadeIn>
-        <Footer />
-	</div>
+        <div
+          className={`w-screen min-h-screen flex flex-col ${
+            user ? 'bg-background-platform' : 'bg-background'
+          }`}
+        >
+          {user ? <AuthenticatedHeader /> : <Header />}
+          <FadeIn>
+            <div className="min-h-[calc(100vh-64px)] flex flex-col justify-center items-center max-w-[500px] w-[90vw] mx-auto">
+              {' '}
+              <h1 className="mb-8 text-4xl text-center">
+                {t('notFound.title')}
+              </h1>
+              <p className="text-center">
+                {t.rich('notFound.message', {
+                  home: (chunks) => (
+                    <Link
+                      href="/"
+                      className="underline underline-offset-4 hover:opacity-80"
+                    >
+                      {chunks}
+                    </Link>
+                  ),
+                  news: (chunks) => (
+                    <Link
+                      href="/news"
+                      className="underline underline-offset-4 hover:opacity-80"
+                    >
+                      {chunks}
+                    </Link>
+                  ),
+                })}
+              </p>
+            </div>
+          </FadeIn>
+          <Footer />
+        </div>
       </SmoothScroll>
     </NextIntlClientProvider>
   );
