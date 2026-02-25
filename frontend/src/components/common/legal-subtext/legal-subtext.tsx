bc2fcfc0b96f3ecde80d50ea6ce6a58d07e7da95
allykeightley <allysonkeightley@outlook.com>
2026-02-24 18:08:37 -0800

    feat: fix legal subtext component

--- frontend/src/components/common/legal-subtext/legal-subtext.tsx
+++ frontend/src/components/common/legal-subtext/legal-subtext.tsx
@@ -1,38 +1,35 @@
 // Copyright © Todd Agriscience, Inc. All rights reserved.
 
-import { Link } from '@/i18n/config';
-import { useTranslations } from 'next-intl';
+import NextLink from 'next/link';
 
 /**
- * Legal subtext component. Renders legal subtext via next-intl.
+ * Legal subtext component. Renders legal subtext.
  *
  * @returns {JSX.Element} - The legal subtext component, containing the account agreement, privacy policy, and terms of service links.
  * */
 export function LegalSubtext() {
-  const t = useTranslations('contactPage');
-
   return (
     <div className="flex flex-col gap-2 mb-5">
       <p className="text-xs font-thin">
-        {t('legal.byContinuing')}{' '}
-        <Link
+        By continuing, you agree to the{' '}
+        <NextLink
           href="/account-agreement.pdf"
           target="_blank"
           className="underline font-normal"
         >
-          {t('legal.accountAgreement')}
-        </Link>{' '}
-        {t('legal.and')}{' '}
-        <Link href="/privacy" className="underline font-normal">
-          {t('legal.privacyPolicy')}
-        </Link>
+          Todd Account Agreement
+        </NextLink>{' '}
+        and{' '}
+        <NextLink href="/privacy" className="underline font-normal">
+          Privacy Policy
+        </NextLink>
         .
       </p>
       <p className="text-xs font-thin">
-        <Link href="/terms" className="underline font-normal">
-          {t('legal.termsOfService')}
-        </Link>{' '}
-        {t('legal.apply')}
+        <NextLink href="/terms" className="underline font-normal">
+          Terms of Service
+        </NextLink>{' '}
+        apply.
       </p>
     </div>
   );
