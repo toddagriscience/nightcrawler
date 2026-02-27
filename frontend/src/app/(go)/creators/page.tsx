c7bb46eae7f765c768e3af46635662fac1938fd6
Gil <gmiranda540@yahoo.com>
2026-02-27 01:00:59 -0500

    Seperate branch so nothing breaks

--- frontend/src/app/(go)/creators/page.tsx
+++ frontend/src/app/(go)/creators/page.tsx
@@ -11,20 +11,7 @@ import Faq from './components/faq/faq';
 export default function CreatorsPage() {
   return (
     <div>
-      {/* FAQ Section */}
-      <Faq />
-      {/* Apply Section */}
-      <div className="w-full h-fit mb-16 md:mb-32 py-12 md:py-16">
-        <Link
-          href="#"
-          className="text-3xl md:text-4xl lg:text-4xl leading-tight font-thin flex justify-center items-center gap-5"
-        >
-          Apply
-          <span className="mt-1">
-            <HiArrowLongRight className="size-12" />
-          </span>
-        </Link>
-      </div>
+      <h1>Creators Subdomain Page</h1>
     </div>
   );
 }
