db7bd9ab5e38da8b39261c863bdf84cdac5d3ee4
Nick Mack <nam7nam14@gmail.com>
2026-03-01 14:49:43 -0500

    style: apply prettier formatting

--- frontend/src/app/(unauthenticated)/[locale]/disclosures/page.tsx
+++ frontend/src/app/(unauthenticated)/[locale]/disclosures/page.tsx
@@ -1,6 +1,5 @@
 import Link from 'next/link';
-import { getTranslations } from "next-intl/server";
-
+import { getTranslations } from 'next-intl/server';
 
 type LinkItem = { label: string; href: string };
 type LinkSections = {
@@ -8,61 +7,60 @@ type LinkSections = {
   subLinks: LinkItem[];
 };
 
-
 function LinkList({ linkItem }: { linkItem: LinkSections }) {
-    return(
-        <ul className="mt-1 space-y-2">
-           {Object.entries(linkItem).map(([section, refLinks]) => (
-              <li key={section} 
-                  className="underline mb-8 flex flex-col gap-2"
-              >
-                 {refLinks.map((link) => (
-                       <Link key={link.label} href={link.href} className="hover:opacity-70 transition duration-200">
-                          {link.label}
-                       </Link>
-                  ))}
-              </li>
-             ))}
-        </ul>
-        
-    );
+  return (
+    <ul className="mt-1 space-y-2">
+      {Object.entries(linkItem).map(([section, refLinks]) => (
+        <li key={section} className="underline mb-8 flex flex-col gap-2">
+          {refLinks.map((link) => (
+            <Link
+              key={link.label}
+              href={link.href}
+              className="hover:opacity-70 transition duration-200"
+            >
+              {link.label}
+            </Link>
+          ))}
+        </li>
+      ))}
+    </ul>
+  );
 }
 
 export default async function DisclosuresPage() {
-  const t = await getTranslations("disclosures");
-  
+  const t = await getTranslations('disclosures');
+
   const generalLinks: LinkSections = {
     primaryLinks: [
-      {label: t('links.general.privacyEnglish'), href: "#"},
-      {label:t('links.general.privacyEU'), href: "#"},
-      {label: t('links.general.privacyJapan'), href: "#"}
+      { label: t('links.general.privacyEnglish'), href: '#' },
+      { label: t('links.general.privacyEU'), href: '#' },
+      { label: t('links.general.privacyJapan'), href: '#' },
     ],
 
-    subLinks: [
-      {label: t("links.general.misuseNameBrand"), href: "#"}
-    ]
+    subLinks: [{ label: t('links.general.misuseNameBrand'), href: '#' }],
   };
 
-  const ukLinks = [
-    {label: t('links.uk.modernSlavery'), href: "#"}
-  ];
+  const ukLinks = [{ label: t('links.uk.modernSlavery'), href: '#' }];
 
-  return(
+  return (
     <main className="h-[90vh] mx-auto my-3 max-w-5xl px-6 py-14">
       <div className="mb-8">
-        <h1 className="mb-4 text-4xl font-medium">{t("title.general")}</h1>
+        <h1 className="mb-4 text-4xl font-medium">{t('title.general')}</h1>
         <LinkList linkItem={generalLinks} />
       </div>
       <div>
-        <h1 className="mb-8 text-4xl font-medium">{t("title.uk")}</h1>
+        <h1 className="mb-8 text-4xl font-medium">{t('title.uk')}</h1>
         <ul className="space-y-2">
-           {ukLinks.map((link) => (
-              <li key={link.label} className="underline hover:opacity-70 transition duration-200" >
-                 <Link href={link.href} className="">
-                    {link.label}
-                 </Link>
-              </li>
-            ))}
+          {ukLinks.map((link) => (
+            <li
+              key={link.label}
+              className="underline hover:opacity-70 transition duration-200"
+            >
+              <Link href={link.href} className="">
+                {link.label}
+              </Link>
+            </li>
+          ))}
         </ul>
       </div>
     </main>
