f43f2e68d599112820f11ea19cdbe98713babea9
kurealnum <oscar.gaske.cs@gmail.com>
2026-03-02 18:14:59 -0500

    refactor: make headers easier to use

--- frontend/src/app/(unauthenticated)/[locale]/(marketing)/components/index.ts
+++ frontend/src/app/(unauthenticated)/[locale]/(marketing)/components/index.ts
@@ -7,3 +7,4 @@ export { default as NewsHighlights } from './news-highlights/news-highlights';
 export { default as LandingPage } from './page/landing-page';
 export { default as Quote } from './quote/quote';
 export { default as ScrollShrinkWrapper } from './scroll-shrink-wrapper/scroll-shrink-wrapper';
+
