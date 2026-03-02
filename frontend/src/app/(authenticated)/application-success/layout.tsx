e964a3375e66f399dd3ad9e8c590183ae68da691
kurealnum <oscar.gaske.cs@gmail.com>
2026-03-02 12:12:39 -0500

    feat: add title to application success

---
+++ frontend/src/app/(authenticated)/application-success/layout.tsx
@@ -0,0 +1,11 @@
+// Copyright © Todd Agriscience, Inc. All rights reserved.
+
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'Application Success | Todd',
+};
+
+export default function Layout({ children }: { children: React.ReactNode }) {
+  return <>{children}</>;
+}
