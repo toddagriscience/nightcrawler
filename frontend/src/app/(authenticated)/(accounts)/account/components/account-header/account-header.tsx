9bad7aeb27ae68acaff3176582969626fba57221
allykeightley <allysonkeightley@outlook.com>
2026-03-03 14:01:54 -0800

    style: adjust header home placement

--- frontend/src/app/(authenticated)/(accounts)/account/components/account-header/account-header.tsx
+++ frontend/src/app/(authenticated)/(accounts)/account/components/account-header/account-header.tsx
@@ -12,13 +12,15 @@ interface AccountHeaderProps {
 export default function AccountHeader({ farmName }: AccountHeaderProps) {
   return (
     <div className="border-b border-[#D9D9D9]">
-      <div className="mx-auto flex w-full max-w-[1300px] items-end gap-22 px-5 mt-16 mb-10">
+      <div className="mx-auto flex w-full max-w-[1300px] items-center gap-22 px-5 mt-16 mb-10">
         <Link
           href="/"
-          className="text-foreground inline-flex items-center gap-2 text-[18px] leading-none font-[400]"
+          className="text-foreground inline-flex items-center gap-2 mt-[-4px]"
         >
           <BiArrowBack className="size-4" />
-          <span className="text-lg leading-none">Home</span>
+          <span className="text-lg leading-none font-normal text-foreground text-lg tracking-tight">
+            Home
+          </span>
         </Link>
         <h1 className="text-foreground text-4xl leading-none font-light">
           {farmName}
