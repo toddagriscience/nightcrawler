9e32cc9fe6536d996b68567bce26e0ff4942d507
allykeightley <allysonkeightley@outlook.com>
2026-03-12 17:44:36 -0700

    style: update platform contact page

--- frontend/src/app/(authenticated)/(misc)/contact/components/contact-links-section.tsx
+++ frontend/src/app/(authenticated)/(misc)/contact/components/contact-links-section.tsx
@@ -19,41 +19,41 @@ const contactLinkItems: ContactLinkItem[] = [
   {
     label: 'Training request',
     href: 'mailto:service.us@toddagriscience.com',
-    icon: <BiBody />,
+    icon: <BiBody className="size-6" />,
   },
   {
-    label: 'Recent Updated',
-    href: '/news',
-    icon: <BiSolidInfoSquare />,
+    label: 'Recent Updates',
+    href: '/en/news',
+    icon: <BiSolidInfoSquare className="size-6" />,
   },
   {
     label: 'Data Deletion Request',
-    href: '/privacy',
-    icon: <BiSolidUserMinus />,
+    href: 'mailto:service.us@toddagriscience.com',
+    icon: <BiSolidUserMinus className="size-6" />,
   },
   {
     label: 'Disclosures',
-    href: '/disclosures',
-    icon: <BiSolidInfoCircle />,
+    href: '/en/disclosures',
+    icon: <BiSolidInfoCircle className="size-6" />,
   },
 ];
 
 export default function ContactLinksSection() {
   return (
-    <section className="mt-12 border-t border-black/20">
+    <section className="mt-10 border-t border-[#D9D9D9]">
       {contactLinkItems.map((item) => (
         <Link
           href={item.href}
-          className="group flex w-full items-center justify-between border-b border-black/20 px-2 py-4 transition-opacity hover:opacity-70"
+          className="group flex w-full items-center min-h-11 justify-between border-b border-[#D9D9D9] px-0.5 py-2 transition-opacity hover:opacity-70"
           key={item.label}
         >
-          <div className="flex items-center gap-3">
+          <div className="flex items-center gap-2">
             {item.icon}
-            <span className="text-foreground text-lg leading-none font-[400]">
+            <span className="text-foreground text-sm font-thin">
               {item.label}
             </span>
           </div>
-          <BiChevronRight className="text-foreground h-7 w-7" />
+          <BiChevronRight className="text-foreground size-6" />
         </Link>
       ))}
     </section>
