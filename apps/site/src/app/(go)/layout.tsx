7357babff76c7a1d5ffab2f7df40b3754035f818
Allyson <168941507+akeight@users.noreply.github.com>
2026-03-18 19:45:20 -0700

    feat: creators page (#783)
    ## Description
    
    Fixes #603 
    
    ## Checklist
    
    - [x] You've included unit or integration tests for your change, where
    applicable.
    - [x] You've included inline docs for your change, where applicable.
    - [x] Any components that you've modified are accessible.
    - [x] You've used [conventional
    commits](https://www.conventionalcommits.org/en/v1.0.0/) where
    appropriate
    
    ---------
    
    Co-authored-by: David Shalshin <128562284+dshal20@users.noreply.github.com>

---
+++ apps/site/src/app/(go)/layout.tsx
@@ -0,0 +1,31 @@
+// Copyright © Todd Agriscience, Inc. All rights reserved.
+
+import GoFooter from '@/components/go/footer/footer';
+import GoHeader from '@/components/go/header/header';
+import { FadeIn, SmoothScroll } from '../../components/common';
+import { fontVariables } from '../../lib/fonts';
+import '../globals.css';
+
+/**
+ * Layout for go subdomain routes
+ * Applies go subdomain background color and includes the go subdomain header and footer
+ * @param {React.ReactNode} children - The children of the layout
+ * @returns {React.ReactNode} - The go subdomain layout
+ */
+export default function GoLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <html lang="en">
+      <body className={fontVariables}>
+        <SmoothScroll>
+          <FadeIn className="min-h-screen flex flex-col">
+            <GoHeader />
+            <div className="flex-grow flex flex-col justify-end">
+              {children}
+            </div>
+            <GoFooter />
+          </FadeIn>
+        </SmoothScroll>
+      </body>
+    </html>
+  );
+}
