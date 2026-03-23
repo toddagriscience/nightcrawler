62355060b905446fb29ad6b1586a56b45d636aa8
allykeightley <allysonkeightley@outlook.com>
2026-03-23 15:35:26 -0700

    style: center auth loading page, add wordmark

--- apps/site/src/app/(authenticated)/loading.tsx
+++ apps/site/src/app/(authenticated)/loading.tsx
@@ -1,17 +1,28 @@
 // Copyright © Todd Agriscience, Inc. All rights reserved.
 
+import ToddHeader from '../../components/common/wordmark/todd-wordmark';
+
 /**
  * This component is used to display a loading state for authenticated paths.
  */
 export default async function DashboardLoading() {
   return (
-    <div className="min-h-screen flex items-center justify-center">
-      <div className="flex flex-col items-center gap-4">
-        <h1 className="text-2xl">
-          Thinking
-          <span className="ellipsis" />
-        </h1>
+    <>
+      <header className="w-full" role="banner">
+        <div className="mx-auto max-w-[107rem] mt-4 px-4 py-4 sm:px-6 lg:px-8">
+          <div className="flex items-center justify-between">
+            <ToddHeader className="flex min-h-10 flex-row items-center" />
+          </div>
+        </div>
+      </header>
+      <div className="h-[calc(100vh-190px)] flex items-center justify-center">
+        <div className="flex flex-col items-center gap-4">
+          <h1 className="text-2xl">
+            Thinking
+            <span className="ellipsis" />
+          </h1>
+        </div>
       </div>
-    </div>
+    </>
   );
 }
