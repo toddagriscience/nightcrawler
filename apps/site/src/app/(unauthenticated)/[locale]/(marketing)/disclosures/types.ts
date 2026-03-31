b79f037797f754011591adc83ea1e808f1ecdb26
Cristian <cristiangramada6@gmail.com>
2026-03-30 19:19:46 -0700

    fix: relocate disclosures page and remove frontend duplicates

--- apps/site/src/app/(unauthenticated)/[locale]/(marketing)/disclosures/types.ts
+++ apps/site/src/app/(unauthenticated)/[locale]/(marketing)/disclosures/types.ts
@@ -1,10 +1,12 @@
 // Copyright © Todd Agriscience, Inc. All rights reserved.
 
+/** A single navigable link with a display label and destination href. */
 export type LinkItem = {
   label: string;
   href: string;
 };
 
+/** A grouped set of disclosure links divided into primary and sub-links. */
 export type LinkSections = {
   primaryLinks: LinkItem[];
   subLinks: LinkItem[];
