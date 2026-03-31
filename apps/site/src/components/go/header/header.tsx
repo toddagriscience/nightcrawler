a9aecc4293ed8a250a005c6e790c9d610daaf1a0
Allyson <168941507+akeight@users.noreply.github.com>
2026-03-31 16:19:40 -0700

    style: header height (#811)

--- apps/site/src/components/go/header/header.tsx
+++ apps/site/src/components/go/header/header.tsx
@@ -11,7 +11,7 @@ export default function GoHeader() {
   return (
     <header className="w-full" role="banner">
       <div className="mx-auto mt-4 px-6 sm:mx-6 md:mx-10 xl:mx-34">
-        <div className="flex items-baseline justify-between sm:h-3 lg:h-5">
+        <div className="flex items-baseline justify-between h-0 md:h-3 lg:h-5">
           <ToddHeader className="flex min-h-10 flex-row items-center" />
           <h1 className="text-xl sm:text-2xl font-normal">Creator Program</h1>
         </div>
