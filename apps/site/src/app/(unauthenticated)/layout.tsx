a0a74a2653952f3a3b85bbbf3832d7278a3c84ec
Vincent Todd <v@toddagriscience.com>
2026-06-06 22:04:03 -0400

    Update description for Todd's metadata profile (#918)
    ## Description
    
    Fixes #{issue number}
    
    ## Checklist
    
    - [ ] You've included unit or integration tests for your change, where
    applicable.
    - [ ] You've included inline docs for your change, where applicable.
    - [ ] Any components that you've modified are accessible.
    - [ ] You've used [conventional
    commits](https://www.conventionalcommits.org/en/v1.0.0/) where
    appropriate

--- apps/site/src/app/(unauthenticated)/layout.tsx
+++ apps/site/src/app/(unauthenticated)/layout.tsx
@@ -41,7 +41,7 @@ const organizationStructuredData: WithContext<Organization> = {
   logo: 'https://toddagriscience.com/icon.png',
   name: 'Todd',
   description:
-    'Todd combines deep experience in regenerative agriculture, farm management and engaging consumers.',
+    'Todd is the frontier research lab dedicated to building foundation technology and crop genetics for sustainable agriculture.',
   founder: {
     '@type': 'Person',
     name: 'Vincent Todd',
